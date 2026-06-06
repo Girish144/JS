e933f5c139838261a7be3c2137745c0db9ae57f0
Girish144 <girishkumbar100@gmail.com>
2026-06-06 20:29:48 +0530

    completed

---
+++ even.js
@@ -0,0 +1,13 @@
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(let i=2;i<=10;i+=2){
+    console.log(i);
+    console.log("even number");
+}
+
+for(let i=1;i<=10;i+=2){
+    console.log(i);
+    console.log("odd number");
+}
