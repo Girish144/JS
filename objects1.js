2c483f35489e8ac8b485a856daa2b8ba9cd71bd2
Girish144 <girishkumbar100@gmail.com>
2026-06-08 11:59:19 +0530

    objects and this is completed

---
+++ objects1.js
@@ -0,0 +1,10 @@
+let student={
+    name:"Girish",
+    age:21,
+    course:"mca",
+
+    display:function(){
+        console.log(this.name+"Graduvated in"+this.course);
+    }
+}
+student.display();
