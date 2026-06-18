13fa9265e7a590252b3dc766d18646a95ac0cbb8
Girish Kumbar <girishkumbar100@gmail.com>
2026-06-18 10:16:01 +0530

    Update Alist.js

--- Alist.js
+++ Alist.js
@@ -2,3 +2,12 @@ let list=[1,2,3,11,22,33,20]
 list.filter(list => list >10);
 
 console.log(list.map(list => list *2));
+
+or
+let list=[1,2,3,11,22,33,20]
+function display(arr){
+  return arr
+  .filter(list => list > 10)
+  .map(list => list *2);
+}
+console.log(display(list))
